b9c5a0998dd66d9ea8e3c48c2853cca39307bb84
LeChiffre2110 <younes.abdelwadoud@outlook.de>
2023-05-24 14:23:37 +0200

    feature: add video player component

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -1,8 +1,10 @@
 import VideoPlayerStyles from './VideoPlayer.module.css'
 
 function VideoPlayer(){
-    return(
-        <h2>VideoPlayer Component</h2>
+    return (
+        <div className="video-container">
+            <video src={props.videoUrl}></video>
+        </div>
     )
 }
 
