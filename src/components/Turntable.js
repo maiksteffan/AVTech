a7d050694f7a65123fba14a27aaf9b01284d0040
LeChiffre2110 <younes.abdelwadoud@outlook.de>
2023-06-09 16:55:49 +0200

    refactor: change logic to use web audio api

--- src/components/Turntable.js
+++ src/components/Turntable.js
@@ -1,15 +1,61 @@
 import "./Turntable.css";
 import sampleSoundwave from "./../assets/soundwave.PNG";
 import sampleImg from "./../assets/sampleImg.png";
-import { useRef, useState, useEffect } from "react";
+import React, { useContext, useEffect, useState } from 'react';
+import {AudioLogicContext} from './../logic/AudioLogicContext';
 
-function Turntable({ id, backgroundImg = "./../assets/sampleImg.png", track, volume }) {
+function Turntable({ id, backgroundImg = "./../assets/sampleImg.png", url }) {
   const [isPaused, setIsPaused] = useState(true);
   const discClass = `disc-${id}`;
+  const audioLogic = useContext(AudioLogicContext);
+  const [audioLoaded, setAudioLoaded] = useState(false);
+  const [audioSource, setAudioSource] = useState(null);
+  const [audioBuffer, setAudioBuffer] = useState(null);
+
+  useEffect(() => {
+    audioLogic.loadAudio(url)
+      .then(audioBuffer => {
+        setAudioBuffer(audioBuffer);
+        setAudioLoaded(true);
+      })
+      .catch(error => {
+        console.error('Error loading audio file:', error);
+      });
+
+    return () => {
+      if (audioSource) {
+        audioLogic.disconnectAudioSource(audioSource);
+      }
+    };
+  }, [audioLogic, url]);
+
+  function createNewSourceNode() {
+    if (!audioLoaded) {
+      return;
+    }
+
+    const sourceNode = audioLogic.audioContext.createBufferSource();
+    sourceNode.buffer = audioBuffer;
+    setAudioSource(sourceNode);
+    audioLogic.connectAudioSource(sourceNode);
+    sourceNode.start();
+  }
 
   function togglePause() {
+    if (!audioLoaded) {
+      return;
+    }
+  
     const disc = document.querySelector(`.${discClass}`);
     disc.classList.toggle("spinning");
+  
+    if (isPaused) {
+      createNewSourceNode();
+    } else {
+      audioSource.stop();
+      audioLogic.disconnectAudioSource(audioSource);
+    }
+    
     setIsPaused(!isPaused);
   }
 
