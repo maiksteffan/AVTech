b8f3caef0011b86319ba6226acc322ffab7f0c57
LeChiffre2110 <younes.abdelwadoud@outlook.de>
2023-06-09 16:42:51 +0200

    refactor: tweaks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import {Routes, Route,Navigate} from 'react-router-dom'
-import {useState} from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useState } from "react";
 import Config from "./pages/ConfigPage";
 import DJTool from "./pages/DjVjPage";
 import DjVjPage from "./pages/DjVjPage";
 import ConfigPage from "./pages/ConfigPage";
-import './App.css';
+import "./App.css";
 
 function App() {
-    return (
-        <div className='app'>
-            <main>
-                <Routes>
-                    <Route path='/dj-vj-tool' element={<DjVjPage/>}/>
-                    <Route path='/config' element={<ConfigPage/>}/>
-                    <Route path='*' element={<Navigate to='/config'/>}/>
-                </Routes>
-            </main>
-        </div>
-    );
+  return (
+    <div className="app">
+      <main>
+          <Routes>
+            <Route path="/dj-vj-tool" element={<DjVjPage />} />
+            <Route path="/config" element={<ConfigPage />} />
+            <Route path="*" element={<Navigate to="/config" />} />
+          </Routes>
+      </main>
+    </div>
+  );
 }
 
 export default App;
