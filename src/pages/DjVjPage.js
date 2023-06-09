7d73daa72415b083c3bf88f850667a37240d1a97
LeChiffre2110 <younes.abdelwadoud@outlook.de>
2023-06-09 18:03:23 +0200

    refactor: change demo sounds

--- src/pages/DjVjPage.js
+++ src/pages/DjVjPage.js
@@ -45,13 +45,13 @@ export default function DJVJTool() {
         <Turntable
           id="left"
           backgroundImg="./../assets/sampleImg.png"
-          url="./../demo_sounds/hiphop_demo.mp3"
+          url="./../demo_sounds/demo1.mp3"
         />
         <VideoPlayer videoUrl="https://media.istockphoto.com/id/1371473504/video/4k-video-of-flowing-binary-code-in-green-color.mp4?s=mp4-640x640-is&k=20&c=Na3PSkNBQsfJY3I0U58mJ25PEeiFUokeQNj1FAoTI8Y=" />
         <Turntable
           id="right"
           backgroundImg="./../assets/sampleImg2.png"
-          url="./../demo_sounds/hiphop_demo.mp3"
+          url="./../demo_sounds/demo2.mp3"
         />
       </div>
       <div className="dj-controller-bottom">
