6d40c573614104cb84680cff7616d82431b33af8
LeChiffre2110 <younes.abdelwadoud@outlook.de>
2023-06-11 00:12:55 +0200

    remove sample data

--- src/pages/DjVjPage.js
+++ src/pages/DjVjPage.js
@@ -8,37 +8,6 @@ import "primereact/resources/primereact.min.css";
 import "./../App.css";
 
 export default function DJVJTool() {
-  const [volumeLeft, setVolumeLeft] = useState(0);
-  const [volumeRight, setVolumeRight] = useState(0);
-  const sampleSongs = [
-    {
-      title: "Baila Baila Baila",
-      artist: "Ozuna",
-      length: "2:37",
-    },
-    {
-      title: "Con Altura",
-      artist: "ROSALÍA, J Balvin",
-      length: "2:41",
-    },
-    { title: "MIA", artist: "Bad Bunny, Drake", length: "3:30" },
-    {
-      title: "Señorita",
-      artist: "Shawn Mendes, Camila Cabello",
-      length: "3:11",
-    },
-    { title: "Despacito", artist: "Luis Fonsi, Daddy Yankee", length: "4:41" },
-    { title: "Vente Pa' Ca", artist: "Ricky Martin, Maluma", length: "4:18" },
-    { title: "Dura", artist: "Daddy Yankee", length: "3:21" },
-    { title: "Tusa", artist: "Karol G, Nicki Minaj", length: "3:21" },
-    { title: "La Modelo", artist: "Ozuna, Cardi B", length: "4:15" },
-    {
-      title: "Me Gusta",
-      artist: "Anitta, Cardi B, Myke Towers",
-      length: "2:44",
-    },
-  ];
-
   return (
     <div>
       <div className="dj-controller-container">
@@ -55,9 +24,9 @@ export default function DJVJTool() {
         />
       </div>
       <div className="dj-controller-bottom">
-        <Tracklist songs={sampleSongs} />
+        <Tracklist />
         <Controller />
-        <Tracklist songs={sampleSongs} />
+        <Tracklist />
       </div>
     </div>
   );
