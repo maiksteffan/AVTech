e738f50d19fec8bbded4a35b18d76dc3faae23d6
LeChiffre2110 <younes.abdelwadoud@outlook.de>
2023-06-14 18:40:33 +0200

    refactor: change to work with buffer instead of url

--- src/components/Turntable.js
+++ src/components/Turntable.js
@@ -1,56 +1,63 @@
 import "./Turntable.css";
 import sampleSoundwave from "./../assets/soundwave.PNG";
 import sampleImg from "./../assets/sampleImg.png";
-import React, { useContext, useEffect, useState } from "react";
+import React, {
+  useContext,
+  useEffect,
+  useState,
+  componentDidUpdate,
+} from "react";
 import { AudioLogicContext } from "./../logic/AudioLogicContext";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlay } from "@fortawesome/free-solid-svg-icons";
 import { faPause } from "@fortawesome/free-solid-svg-icons";
 import { faRotate } from "@fortawesome/free-solid-svg-icons";
 
-function Turntable({ id, backgroundImg = "./../assets/sampleImg.png", url }) {
+function Turntable({
+  id,
+  backgroundImg = "./../assets/sampleImg.png",
+  buffer,
+}) {
   const [isPaused, setIsPaused] = useState(true);
   const [isSynchronized, setIsSynchronized] = useState(false);
   const discClass = `disc-${id}`;
   const audioLogic = useContext(AudioLogicContext);
   const [audioLoaded, setAudioLoaded] = useState(false);
   const [audioSource, setAudioSource] = useState(null);
-  const [audioBuffer, setAudioBuffer] = useState(null);
+  const [audioBuffer, setAudioBuffer] = useState(audioLogic.getAudioBuffer(id));
   const [bpm, setBpm] = useState(0);
 
   useEffect(() => {
-    audioLogic
-      .loadAudio(url)
-      .then((audioBuffer) => {
-        setAudioBuffer(audioBuffer);
-        setAudioLoaded(true);
-        audioLogic.setAudioBuffer(audioBuffer, id);
-      })
-      .catch((error) => {
-        console.error("Error loading audio file:", error);
-      });
+    //stop audioSource when song is changed while playing
+    if (audioSource) {
+      audioSource.stop();
+      audioLogic.disconnectAudioSource(audioSource);
+      setIsPaused(true);
+      const disc = document.querySelector(`.${discClass}`);
+      disc.classList.toggle("spinning");
+    }
 
-    return () => {
-      if (audioSource) {
-        audioLogic.disconnectAudioSource(audioSource);
-      }
-    };
-  }, [audioLogic, url]);
+    setAudioBuffer(buffer);
+    if (audioBuffer) {
+      setAudioLoaded(true);
+      return;
+    }
+  }, [buffer]);
 
   function createNewSourceNode() {
-    if (!audioLoaded) {
+    if (!audioLoaded || !audioBuffer) {
       return;
     }
 
     const sourceNode = audioLogic.audioContext.createBufferSource();
-    sourceNode.buffer = audioBuffer;
+    sourceNode.buffer = audioBuffer.buffer;
     setAudioSource(sourceNode);
     audioLogic.connectAudioSource(sourceNode, id);
     sourceNode.start();
   }
 
   function togglePause() {
-    if (!audioLoaded) {
+    if (!audioLoaded || !buffer) {
       return;
     }
 
@@ -60,7 +67,7 @@ function Turntable({ id, backgroundImg = "./../assets/sampleImg.png", url }) {
     if (isPaused) {
       createNewSourceNode();
 
-      let promise = audioLogic.getBPM(audioBuffer);
+      let promise = audioLogic.getBPM(audioBuffer.buffer);
       promise.then((bpm) => {
         setBpm(bpm.bpm);
       });
@@ -72,14 +79,21 @@ function Turntable({ id, backgroundImg = "./../assets/sampleImg.png", url }) {
     setIsPaused(!isPaused);
   }
 
+  //match the bpm of this audio source to that of the other
   function sync() {
     if (!audioLoaded) {
       return;
     }
 
+    //Call the matchBpm function in the audio logic
     audioLogic.matchBpm(id, audioSource)
-
-    setIsSynchronized(!isSynchronized);
+    .then(targetBpm => {
+      setBpm(targetBpm);
+      setIsSynchronized(!isSynchronized);
+    })
+    .catch(error => {
+      console.error("Error:", error);
+    });
   }
 
   return (
@@ -99,10 +113,10 @@ function Turntable({ id, backgroundImg = "./../assets/sampleImg.png", url }) {
         <h1 className="digital mx-[20px]">{bpm} BPM</h1>
         <div className="round-button" onClick={sync}>
           {!isSynchronized && <FontAwesomeIcon icon={faRotate} size="xl" />}
-          {isSynchronized && (
-            <FontAwesomeIcon icon={faRotate} size="xl" spin/>          )}
+          {isSynchronized && <FontAwesomeIcon icon={faRotate} size="xl" spin />}
         </div>
       </div>
+      {audioBuffer && <h1>Currently Playing: {audioBuffer.name}</h1>}
     </div>
   );
 }
