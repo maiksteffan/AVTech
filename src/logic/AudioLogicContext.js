f70da7dec408fe89cd8898e80c052c6894aa1724
LeChiffre2110 <younes.abdelwadoud@outlook.de>
2023-07-09 15:35:45 +0200

    fix audio not playing bug

--- src/logic/AudioLogicContext.js
+++ src/logic/AudioLogicContext.js
@@ -1,10 +1,23 @@
 import React, { createContext, useState } from 'react';
 import AudioLogic from './AudioLogic';
+import { Button } from "primereact/button";
 
 export const AudioLogicContext = createContext();
 
 export const AudioLogicProvider = ({ children }) => {
-  const [audioLogic] = useState(new AudioLogic());
+  const [audioLogic, setAudioLogic] = useState(null);
+
+  const startAudio = () => {
+    setAudioLogic(new AudioLogic());
+  }
+
+  if (!audioLogic) {
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        <Button className='w-[300px]' onClick={startAudio}  label="Start" outlined />
+      </div>
+    );
+  }
 
   return (
     <AudioLogicContext.Provider value={audioLogic}>
