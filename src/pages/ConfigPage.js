530c7bd961ecb864801838fd97805392b1972bb9
LeChiffre2110 <younes.abdelwadoud@outlook.de>
2023-07-15 18:09:33 +0200

    add function to load demo songs on start

--- src/pages/ConfigPage.js
+++ src/pages/ConfigPage.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { FileUpload } from "primereact/fileupload";
 import { AudioLogicContext } from "./../logic/AudioLogicContext";
 import { Link } from "react-router-dom";
@@ -7,6 +7,20 @@ import { Button } from "primereact/button";
 export default function () {
   const audioLogic = useContext(AudioLogicContext);
 
+  const loadDemoFiles = (urls) => {
+    for (let url of urls) {
+      fetch(url)
+        .then((response) => response.blob())
+        .then((blob) => {
+          const fileName = url.substring(url.lastIndexOf("/") + 1);
+          const file = new File([blob], fileName, { type: blob.type });
+          audioLogic.loadAudioFile(file).then((audioBuffer) => {
+            console.log("Audio file loaded!");
+          });
+        });
+    }
+  };
+
   const handleUpload = (event) => {
     const files = Array.from(event.files);
     handleSaveFiles(files);
@@ -14,13 +28,18 @@ export default function () {
 
   const handleSaveFiles = (files) => {
     files.forEach((file) => {
-      audioLogic.loadAudioFile(file)
+      audioLogic.loadAudioFile(file);
     });
   };
 
+  useEffect(() => {
+    let urls = ["/demo_sounds/demo1.mp3", "/demo_sounds/demo2.mp3"];
+    loadDemoFiles(urls);
+  }, []);
+
   return (
     <div className="w-[80%] h-[90%] m-auto">
-      <h1 className="text-4xl font-bold green-text mb-10">Config Page</h1>
+      <h1 className="mb-10 text-4xl font-bold green-text">Config Page</h1>
       <FileUpload
         name="songs[]"
         url={"/demo_sounds"}
@@ -35,8 +54,10 @@ export default function () {
         }
       />
       <div className="mt-10">
-        <Link to="/dj-vj-tool"><Button label="Continue" outlined /></Link>
-        </div>
+        <Link to="/dj-vj-tool">
+          <Button label="Continue" outlined />
+        </Link>
+      </div>
     </div>
   );
 }
