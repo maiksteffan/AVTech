b4715ae1d575b14c441bc1e1e81273712a9d4269
LeChiffre2110 <younes.abdelwadoud@outlook.de>
2023-07-24 17:20:49 +0200

    add filter changing while playing

--- src/pages/DjVjPage.js
+++ src/pages/DjVjPage.js
@@ -5,6 +5,7 @@ import Controller from "./../components/Controller";
 import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
 import "./../App.css";
+import Filters from "./../logic/Filters";
 import { AudioLogicContext } from "./../logic/AudioLogicContext";
 
 export default function DJVJTool() {
@@ -14,31 +15,77 @@ export default function DJVJTool() {
   const [songLeftPaused, setSongLeftPaused] = useState(true);
   const [songRightPaused, setSongRightPaused] = useState(true);
   const [crossfade, setCrossfade] = useState(0);
+  const [filter, setFilter] = useState("");
 
   //Video player functions
-  
+
   /**
    * Custom hook that controls the video of a song.
-   * @param {boolean} songPaused 
-   * @param {String} channel 
+   * @param {boolean} songPaused boolean that indicates if the song is paused or not
+   * @param {String} channel the channel of the song (left or right)
    */
   const useVideoControl = (songPaused, channel) => {
     useEffect(() => {
       if (songPaused) {
         stopVideo(channel);
       } else {
+        const video =
+          channel === "left"
+            ? document.querySelector("#left video")
+            : document.querySelector("#right video");
+        const canvas =
+          channel === "left"
+            ? document.querySelector("#canvas-left")
+            : document.querySelector("#canvas-right");
+
+        canvas.width = (40 * window.innerWidth) / 100;
+        canvas.height = (23 * window.innerWidth) / 100;
         restartVideo(channel);
         playVideo(channel);
+        drawVideoOnCanvas(video, canvas, filter); // Change filter as desired
       }
     }, [songPaused, channel]);
   };
+
+  /**
+   * Function that draws the video of a song on a canvas.
+   * @param {video} videoElement reference to the video element of the song
+   * @param {canvas} canvasElement reference to the canvas element
+   * @param {Filters} filter the filter to be applied to the video
+   */
+  function drawVideoOnCanvas(videoElement, canvasElement, filter) {
+    if (videoElement.readyState === videoElement.HAVE_ENOUGH_DATA) {
+      const context = canvasElement.getContext("2d");
+      context.clearRect(0, 0, canvasElement.width, canvasElement.height);
+      context.drawImage(
+        videoElement,
+        0,
+        0,
+        canvasElement.width,
+        canvasElement.height
+      );
+
+      // apply filter if it exists in the Filters object
+      if (Filters[filter]) {
+        Filters[filter](context, canvasElement);
+      }
+    }
+
+    // request next frame
+    requestAnimationFrame(() =>
+      drawVideoOnCanvas(videoElement, canvasElement, filter)
+    );
+  }
 
   /**
    * Function that plays the video of a song.
    * @param {string} channel the channel of the song (left or right)
    */
   function playVideo(channel) {
-    const video = channel === "left" ? document.querySelector("#left video") : document.querySelector("#right video");
+    const video =
+      channel === "left"
+        ? document.querySelector("#left video")
+        : document.querySelector("#right video");
     if (!video) {
       return;
     }
@@ -50,7 +97,10 @@ export default function DJVJTool() {
    * @param {string} channel the channel of the song (left or right)
    */
   function stopVideo(channel) {
-    const video = channel === "left" ? document.querySelector("#left video") : document.querySelector("#right video");
+    const video =
+      channel === "left"
+        ? document.querySelector("#left video")
+        : document.querySelector("#right video");
     if (!video) {
       return;
     }
@@ -62,7 +112,10 @@ export default function DJVJTool() {
    * @param {string} channel the channel of the song (left or right)
    */
   function restartVideo(channel) {
-    const video = channel === "left" ? document.querySelector("#left video") : document.querySelector("#right video");
+    const video =
+      channel === "left"
+        ? document.querySelector("#left video")
+        : document.querySelector("#right video");
     video.currentTime = 0;
   }
 
@@ -72,13 +125,13 @@ export default function DJVJTool() {
 
   /**
    * Function that returns the cosine ease of a value.
-   * @param {*} x the value to be eased 
-   * @returns the eased value 
+   * @param {*} x the value to be eased
+   * @returns the eased value
    */
   function cosineEase(x) {
     return (1 - Math.cos(Math.PI * x)) / 2;
   }
-  
+
   /**
    * useEffect hook that changes the opacity of the videos depending on the crossfade value, using cosine ease.
    */
@@ -86,13 +139,39 @@ export default function DJVJTool() {
     // change opacity of videos depending on crossfade
     const videoLeft = document.querySelector("#left video");
     const videoRight = document.querySelector("#right video");
-    if (!videoLeft || !videoRight) {
+    const canvasLeft = document.querySelector("#canvas-left");
+    const canvasRight = document.querySelector("#canvas-right");
+    if (!videoLeft || !videoRight || !canvasLeft || !canvasRight) {
       return;
     }
     videoLeft.style.opacity = 1 - cosineEase(crossfade);
     videoRight.style.opacity = cosineEase(crossfade);
+    canvasLeft.style.opacity = 1 - cosineEase(crossfade);
+    canvasRight.style.opacity = cosineEase(crossfade);
   }, [crossfade]);
+
+  //useEffect that changes the filter when the filter state changes
+  useEffect(() => {
+    // if there are no songs loaded return
+    if (!songLeft && !songRight) {
+      return;
+    }
+
+    // get the videos and canvases of the songs
+    const videoLeft = document.querySelector("#left video");
+    const videoRight = document.querySelector("#right video");
+    const canvasLeft = document.querySelector("#canvas-left");
+    const canvasRight = document.querySelector("#canvas-right");
+
+    // restart the drawing of the videos on the canvases with the new filter, if the videos and canvases exist
+    if (videoLeft && canvasLeft) {
+      drawVideoOnCanvas(videoLeft, canvasLeft, filter);
+    }
+    if (videoRight && canvasRight) {
+      drawVideoOnCanvas(videoRight, canvasRight, filter);
+    }
   
+  }, [filter]);
 
   return (
     <div>
@@ -104,14 +183,30 @@ export default function DJVJTool() {
           setSongPaused={setSongLeftPaused}
         />
         <div id="left" className="relative w-[40vw] h-[25vw]">
+          <div className="flex flex-row items-center justify-center gap-5">
+            <button onClick={() => setFilter("grayscaleFilter")} className="px-3 py-1 border border-gray-700 rounded-md hover:bg-slate-800">
+              B/W
+            </button>
+            <button onClick={() => setFilter("colorInvertFilter")} className="px-3 py-1 border border-gray-700 rounded-md hover:bg-slate-800">
+              Invert
+            </button>
+            <button onClick={() => setFilter("sepiaFilter")} className="px-3 py-1 border border-gray-700 rounded-md hover:bg-slate-800">
+              Sepia
+            </button>
+            <button onClick={() => setFilter("barbieFilter")} className="px-3 py-1 border border-gray-700 rounded-md hover:bg-slate-800">
+              Barbie
+            </button>
+          </div>
           {songLeft && (
-            <div className="absolute top-0 left-0">
-              <video src={songLeft.video} muted loop />
+            <div className="absolute left-0 top-12">
+              <video className="hidden" src={songLeft.video} muted loop />
+              <canvas id="canvas-left" className="w-full h-full" />
             </div>
           )}
           {songRight && (
-            <div id="right" className="absolute top-0 left-0">
-              <video src={songRight.video} muted loop />
+            <div id="right" className="absolute left-0 top-12">
+              <video className="hidden" src={songRight.video} muted loop />
+              <canvas id="canvas-right" />
             </div>
           )}
         </div>
